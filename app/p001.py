4fdfa0455478f04bffcb13a73c96b9457754b002
halfrice <neel@surrus.net>
2017-09-24 17:11:22 -0700

    init

---
+++ app/p001.py
@@ -0,0 +1,12 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def m3or5(limit):
+  s = 0
+  for i in range(1,limit):
+    if i%3 == 0 or i%5 == 0: s += i
+  return(s)
+
+print('Sum:',m3or5(1000))
---
+++ app/p003.py
@@ -0,0 +1,11 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+def isPrime(n):
+  if n%2 == 0 and n > 3: return False
+
+
+def pfac(n):
+
+
