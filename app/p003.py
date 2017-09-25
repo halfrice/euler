09f5f1c8b61dd6c6e65ae8ed7e6bd812998d33f8
halfrice <neel@surrus.net>
2017-09-24 17:50:21 -0700

    largest prime factor

--- app/p003.py
+++ app/p003.py
@@ -3,9 +3,16 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 def isPrime(n):
-  if n%2 == 0 and n > 3: return False
-
+  if n%2 == 0 and n > 2: return False
+  for i in range(3, int(n**0.5)+1, 2):
+    if n%i == 0: return False
+  return True
 
 def pfac(n):
+  primefacs = []
+  for i in range(3,int(n**0.5)+1,2):
+    if isPrime(i) and n%i == 0:
+      primefacs.append(i)
+  return primefacs[-1] # return largest prime factor
 
-
+print(pfac(600851475143))
